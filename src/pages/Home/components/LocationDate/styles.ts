50382efc7193b81d51b20a19b91ae692bd262854
xd-elon <alandevelope@gmail.com>
2022-05-20 18:34:11 -0300

    finish UI update

--- src/pages/Home/components/LocationDate/styles.ts
+++ src/pages/Home/components/LocationDate/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components/native';
 export const LocationDateContainer = styled.View`
   width: 100%;
   height: 10%;
-  border: 1px solid red;
   display: flex;
   flex-direction: column;
   align-items: center;
